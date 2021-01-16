ba384888b838879477311a782539a98c0312b816
Kartanovicius <donatas.karta@gmail.com>
2021-01-16 08:59:41 +0200

    Added navbar buttons

--- js/navbar_button.js
+++ js/navbar_button.js
@@ -1,14 +1,32 @@
 function nav_scroll_profile() {
     var elmnt = document.getElementById("profileid");
     elmnt.scrollIntoView({
-        behavior: 'auto',
-        block: 'center',
-        inline: 'center'
+        behavior: "smooth",
+        block: "end",
+        inline: "start"
     });
 }
 
 function nav_scroll_timeline() {
     var elmnt = document.getElementById("timelineid");
+    elmnt.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    });
+}
+
+function nav_scroll_abilities() {
+    var elmnt = document.getElementById("abilitiesid");
+    elmnt.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    });
+}
+
+function nav_scroll_project() {
+    var elmnt = document.getElementById("projectsid");
     elmnt.scrollIntoView({
         behavior: "smooth",
         block: "end",
