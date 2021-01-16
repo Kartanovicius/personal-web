1c9adf48c539db8a18982ea6ba08f487ea01e5ed
Kartanovicius <donatas.karta@gmail.com>
2021-01-17 00:59:18 +0200

    navigation fix

--- js/navbar_button.js
+++ js/navbar_button.js
@@ -1,35 +1,21 @@
+var offset = 40;
+
 function nav_scroll_profile() {
-    var elmnt = document.getElementById("profileid");
-    elmnt.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "start"
-    });
+    var el = document.querySelector('.profile'); // element you are scrolling to
+    window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 
 function nav_scroll_timeline() {
-    var elmnt = document.getElementById("timelineid");
-    elmnt.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest"
-    });
+    var el = document.querySelector('.timeline'); // element you are scrolling to
+    window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 
 function nav_scroll_abilities() {
-    var elmnt = document.getElementById("abilitiesid");
-    elmnt.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest"
-    });
+    var el = document.querySelector('.abilities'); // element you are scrolling to
+    window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 
-function nav_scroll_project() {
-    var elmnt = document.getElementById("projectsid");
-    elmnt.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest"
-    });
+function nav_scroll_projects() {
+    var el = document.querySelector('.projects'); // element you are scrolling to
+    window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
