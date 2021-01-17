cf5b1e2f312f23576363500c7ab3f3f47a926f65
Kartanovicius <donatas.karta@gmail.com>
2021-01-17 02:12:58 +0200

    Full screen side navigation

--- js/navbar_button.js
+++ js/navbar_button.js
@@ -3,19 +3,23 @@ var offset = 40;
 function nav_scroll_profile() {
     var el = document.querySelector('.profile'); // element you are scrolling to
     window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
+    closeNav();
 }
 
 function nav_scroll_timeline() {
     var el = document.querySelector('.timeline'); // element you are scrolling to
     window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
+    closeNav();
 }
 
 function nav_scroll_abilities() {
     var el = document.querySelector('.abilities'); // element you are scrolling to
     window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
+    closeNav();
 }
 
 function nav_scroll_projects() {
     var el = document.querySelector('.projects'); // element you are scrolling to
     window.scroll({ top: (el.offsetTop - offset), left: 0, behavior: 'smooth' });
+    closeNav();
 }
