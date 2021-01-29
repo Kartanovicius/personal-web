931cb474f0b0b7506a4afcc1ccd38c4e7e645a80
Kartanovicius <donatas.karta@gmail.com>
2021-01-29 06:26:20 +0200

    abilities_switch and minor fix

---
+++ js/abilities_switch.js
@@ -0,0 +1,15 @@
+function abilities_html() {
+    document.getElementById("textbox_text").innerText = "kek";
+}
+
+function abilities_js() {
+    document.getElementById("textbox_text").innerText = "lol";
+}
+
+function abilities_java() {
+    document.getElementById("textbox_text").innerText = "ceburek";
+}
+
+function abilities_csharp() {
+    document.getElementById("textbox_text").innerText = "saslik";
+}
