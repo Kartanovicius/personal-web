718956e296896af5dde9b803a09baf938a3f0211
Kartanovicius <donatas.karta@gmail.com>
2021-02-05 00:53:15 +0200

    Text edit

--- js/abilities_switch.js
+++ js/abilities_switch.js
@@ -1,33 +1,20 @@
 function abilities_html() {
-    document.getElementById("textbox_text").innerText = "kek";
-}
-
-function abilities_js() {
-    document.getElementById("textbox_text").innerText = "lol";
+    document.getElementById("textbox_text").innerHTML = "Knowledge of HTML5, CSS3 and Javascript.<br><br>Experience in working with Git.<br><br>Adobe Photoshop image editing skills.<br><br>Designing and prototyping with Figma.";
 }
 
 function abilities_java() {
-    document.getElementById("textbox_text").innerText = "ceburek";
+    document.getElementById("textbox_text").innerHTML = "Half-year experience in java development.<br><br>Knowledge of Object-Oriented Programming and design.<br><br>Knowledge of relational databases, SQL.";
 }
 
 function abilities_csharp() {
-    document.getElementById("textbox_text").innerText = "saslik";
-}
-
-function bg_color_trigger() {
-    $(document).ready(function() {
-        $(".active_ability").trigger("click");
-    });
+    document.getElementById("textbox_text").innerHTML = "Have acquired skills of C and C# programing languages.<br><br>Experience designing and developing Winforms applications.";
 }
 
 function bg_color() {
-    $(document).ready(function() {
-
-        $('li > button.list')
-            .click(function(e) {
-                $('li > button.list')
-                    .removeClass('active_ability');
-                $(this).addClass('active_ability');
-            });
-    });
+    $('li > button.list')
+        .click(function(e) {
+            $('li > button.list')
+                .removeClass('active_ability');
+            $(this).addClass('active_ability');
+        });
 }
